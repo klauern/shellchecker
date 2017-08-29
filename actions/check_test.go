a460623b17502c6db8f7011cce41f3a6c0c890bb
Nick Klauer <klauer@gmail.com>
2017-08-28 19:00:23 -0500

    stub out check_test

--- actions/check_test.go
+++ actions/check_test.go
@@ -1,1 +1,17 @@
-package actions
+package actions_test
+
+func (as *ActionSuite) Test_CheckShellScripts() {
+
+	var checkScriptTests = []struct {
+		inFile      string
+		expectError bool
+		errorCount  int
+	}{}
+
+	for _, tt := range checkScriptTests {
+		if tt.expectError {
+			as.JSON("/check").Post(tt.inFile)
+		}
+	}
+
+}
